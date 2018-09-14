d61189f5dcdc1d7b3787d48f42da27506687f01c
QUESTSYS\MHenry <mark_henry@questsys.com>
2018-09-14 15:14:33 -0700

    Working Prime Number example

--- Program.cs
+++ Program.cs
@@ -6,7 +6,26 @@ namespace chapter2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int potentialPrime = 2; potentialPrime < 100; potentialPrime++)
+            {
+                if (IsPrime(potentialPrime))
+                {
+                    Console.Write(potentialPrime);
+                    Console.Write(","); 
+                }
+                
+            }
+        }
+        private static bool IsPrime(int potentialPrime)
+        {
+            for (int divisor = 2; divisor < potentialPrime; divisor++)
+            {
+                if (potentialPrime % divisor == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
